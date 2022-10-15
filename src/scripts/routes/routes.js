47379a8926c80f12f4879b9e884ba71c4c7818c2
ersalomo <95175703+ersalomo@users.noreply.github.com>
2022-10-15 12:02:06 +0700

    error

--- src/scripts/routes/routes.js
+++ src/scripts/routes/routes.js
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-import NowPlaying from '../views/pages/now-playing.js';
-import Upcoming from '../views/pages/upcoming.js';
-import Detail from '../views/pages/detail.js';
-
-const routes = {
-  '/': NowPlaying,
-  '/now-playing': NowPlaying,
-  '/upcoming': Upcoming,
-  '/detail/:id': Detail,
-=======
 import '../components/ListItemRestaurant.js';
 import '../components/DetailRestaurant.js';
 import '../components/LikeRestaurant.js';
@@ -22,7 +11,6 @@ const routes = {
   '/list': listItemRestaurant,
   '/favorite': likeRestaurant,
   '/detail/:id': detailRestaurant,
->>>>>>> e9b283177f0ea83f08a3609a56d11a57c9bc1694
 };
 
 export default routes;
